d615e78304c7b8549c631b130ad4cd25c5ed05d9
rafael boas <rafaelrvb@gmail.com>
2021-08-18 15:44:23 -0300

    Create done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,7 +8,17 @@ class RestaurantsController < ApplicationController
   end
 
   def create
+    @restaurant = Restaurant.create(restaurant_params)
+    redirect_to restaurants_path
+  end
 
+  private
+
+  def find_restaurant
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
   end
 
 end
