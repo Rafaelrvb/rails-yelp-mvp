0ff0623e7b0e07782c1d3e8e85160cef03ac4cdc
rafael boas <rafaelrvb@gmail.com>
2021-08-18 15:03:38 -0300

    Inserting 10 restos to DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning #{Restaurant.count} restaurants from the database"
+Restaurant.destroy_all
+puts "DB cleaned"
+
+10.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: Restaurant::RESTAURANT_CATEOGORIES.sample
+  )
+  puts "Restaurant: #{restaurant.id} - #{restaurant.name} created"
+end
+
+puts "done #{Restaurant.count} resto created"
